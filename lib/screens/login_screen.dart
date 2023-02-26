5fd0b85b1d71b3e39f970f0decdecba2edee8bd5
Rahul-519 <rgavit519@gmail.com>
2023-02-26 16:05:26 +0530

    Firebase SignUp Auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/resources/auth_methods.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/utils.dart';
 import 'package:instagram_flutter/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,12 +15,29 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+
+    if (res == "success") {
+    } else {
+      showSnakBar(res, context);
+    }
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -33,25 +52,23 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               Flexible(child: Container(), flex: 2),
 
-
               //svg image
-              SvgPicture.asset('assets/ic_instagram.svg', 
-              color: primaryColor, 
-              height: 64,
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
               ),
-              const SizedBox(
-                height: 64
-                ),
+              const SizedBox(height: 64),
 
               //text field input for email
               TextFieldInput(
                 hintText: "Enter Your Email",
                 textInputType: TextInputType.emailAddress,
                 textEditingController: _emailController,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
 
               //text field input for password
               TextFieldInput(
@@ -59,25 +76,32 @@ class _LoginScreenState extends State<LoginScreen> {
                 textInputType: TextInputType.text,
                 textEditingController: _passwordController,
                 isPass: true,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
 
               // button login
               InkWell(
+                onTap: loginUser,
                 child: Container(
-                  child: const Text("Log in"),
+                  child: _isLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
+                          ),
+                        )
+                      : const Text("Log in"),
                   width: double.infinity,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
                   decoration: const ShapeDecoration(
                     shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
                       ),
-                  ),
-                  color: blueColor,
+                    ),
+                    color: blueColor,
                   ),
                 ),
               ),
@@ -97,13 +121,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   GestureDetector(
-                    onTap: (){},
+                    onTap: () {},
                     child: Container(
-                      child: const Text("Sign up.", 
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                        ),
-                        ),
+                      child: const Text(
+                        "Sign up.",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
                       padding: const EdgeInsets.symmetric(
                         vertical: 8,
                       ),
