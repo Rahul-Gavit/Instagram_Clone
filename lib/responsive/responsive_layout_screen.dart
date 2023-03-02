180eda3058a54259a4cba26afd8ee5d83dc9c74e
Rahul-519 <rgavit519@gmail.com>
2023-03-02 14:38:04 +0530

    Mobile Bottom App Bar

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/utils/dimentions.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   final Widget webScrrenLayout;
   final Widget mobileScreenLayout;
   const ResponsiveLayout({
@@ -11,15 +13,31 @@ class ResponsiveLayout extends StatelessWidget {
     }) : super(key: key);
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints){
         if(constraints.maxHeight > webScreenSize){
           //web Screen Layout
-          return webScrrenLayout;
+          return widget.webScrrenLayout;
         }
         //Mobile Screen Layout
-        return mobileScreenLayout;
+        return widget.mobileScreenLayout;
     },
     );
   }
